fff75a1959fb99770059de43eeaa00b40dcd45f4
go-tiger <inter_tiger_@naver.com>
2024-04-26 00:08:02 +0900

    feat: typeorm DB 수정 - typeorm 세팅 수정 - 환경 변수에 값 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => ({
                 type: 'postgres',
-                host: configService.get<string>('DB_HOST'),
-                port: configService.get<number>('DB_PORT'),
-                username: configService.get<string>('DB_USERNAME'),
-                password: configService.get<string>('DB_PASSWORD'),
-                database: configService.get<string>('DB_DATABASE'),
+                url: configService.get<string>('DB_URL'),
                 synchronize: true,
                 logging: true,
             }),
