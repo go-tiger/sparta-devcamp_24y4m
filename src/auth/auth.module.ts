c88e6e317edff86571fec5a23c71f2e14895749f
go-tiger <inter_tiger_@naver.com>
2024-04-29 23:40:43 +0900

    feat: auth 컨트롤러, 서비스 생성 및 주입

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
 
 @Module({
     imports: [TypeOrmModule],
-    controllers: [],
-    providers: [],
+    controllers: [AuthController],
+    providers: [AuthService],
     exports: [],
 })
 export class AuthModule {}
