3880dfed3f2f151ea2623cff342f139dbe493c3e
go-tiger <inter_tiger_@naver.com>
2024-04-30 22:04:12 +0900

    feat: 회원가입 기능 구현

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from '../services/auth.service';
+import { CreateUserDto } from '../dtos/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authservice: AuthService) {}
+
+    @Post('signup')
+    async createUser(@Body() dto: CreateUserDto) {
+        return await this.authservice.createUser(dto);
+    }
+}
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { CreateUserDto } from '../dtos/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) {}
+
+    async createUser(user: CreateUserDto) {
+        const findUser = await this.userRepository.findOneBy({ email: user.email });
+
+        if (findUser) {
+            throw new ConflictException('이미 존재하는 회원입니다.');
+        }
+
+        return this.userRepository.save(user);
+    }
+}
