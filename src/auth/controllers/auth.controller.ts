d44c62aedfc48953c7d92b5952fc1945c62b4523
go-tiger <inter_tiger_@naver.com>
2024-05-02 21:32:41 +0900

    feat: 로그인 기능 구현

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { LoginDto } from '../dtos/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
     @Post('signup')
     async createUser(@Body() dto: CreateUserDto) {
         return await this.authservice.createUser(dto);
+    }
+
+    @Post('login')
+    async login(@Body() dto: LoginDto) {
+        return await this.authservice.login(dto);
     }
 }
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,15 +1,18 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from '../dtos/login.dto';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(User)
         private userRepository: Repository<User>,
+        private jwtService: JwtService,
     ) {}
 
     async createUser(user: CreateUserDto) {
@@ -22,5 +25,19 @@ export class AuthService {
         user.password = await argon2.hash(user.password);
 
         return this.userRepository.save(user);
+    }
+
+    async login(user: LoginDto) {
+        const { email, password } = user;
+        const userData = await this.userRepository.findOneBy({ email });
+
+        if (userData && (await argon2.verify(userData.password, password))) {
+            const payload = { id: userData.id };
+            const accessToken = await this.jwtService.sign(payload);
+
+            return { accessToken };
+        } else {
+            throw new UnauthorizedException('로그인 실패');
+        }
     }
 }
