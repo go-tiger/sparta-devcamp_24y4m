38ccfd9ec047f0f9430bdd4e69e4047228bf62eb
go-tiger <inter_tiger_@naver.com>
2024-05-02 20:47:37 +0900

    feat: jwt 관련 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [
+        TypeOrmModule.forFeature([User]),
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            secret: 'SecretKey',
+            signOptions: {
+                expiresIn: 3600,
+            },
+        }),
+    ],
     controllers: [AuthController],
     providers: [AuthService],
     exports: [],
